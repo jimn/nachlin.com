988a60f5061bba3a29a8e86174729fd454e6e8bb
Jim Nachlin <431065+jimn@users.noreply.github.com>
2025-11-13 13:40:57 -0500

    Delete bicycles directory

--- bicycles/ndxz-admin/config/config.php
+++
@@ -1,9 +0,0 @@
-<?php  if (!defined('SITE')) exit('No direct script access allowed');
-
-$indx['db'] 		= 'indexhibit';
-$indx['user'] 		= 'indexhibit';
-$indx['pass'] 		= 'amazingly amazing';
-$indx['host'] 		= 'localhost';
-$indx['sql']		= 'mysql';
-
-?>
